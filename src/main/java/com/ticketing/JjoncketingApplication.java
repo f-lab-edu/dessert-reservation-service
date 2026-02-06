51f95787c39d86e1618de3b78ccae702573aa032
newdana01 <newdana01@gmail.com>
2026-02-06 16:52:24 +0900

    initial setting: 프로젝트 구조 폴더 생성

--- src/main/java/com/ticketing/JjoncketingApplication.java
+++ src/main/java/com/ticketing/JjoncketingApplication.java
@@ -1,4 +1,4 @@
-package com.ticketing.jjoncketing;
+package com.ticketing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
