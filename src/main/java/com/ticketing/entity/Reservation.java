b060bb5f66d94ee36f36f639c978a6405ae3882d
newdana01 <newdana01@gmail.com>
2026-02-12 21:24:22 +0900

    feat: Reservation Entity 추가

---
+++ src/main/java/com/ticketing/entity/Reservation.java
@@ -0,0 +1,30 @@
+package com.ticketing.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "reservations")
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class Reservation {
+    @Id
+    @Column(name = "reservation_id")
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Dessert dessert;
+    private Integer count;
+    private Integer totalPrice;
+    @Column(name = "reserve_status")
+    private String reserveStatus;
+    @Column(name = "created_dt")
+    @CreatedDate
+    private LocalDateTime createdDt;
+}
