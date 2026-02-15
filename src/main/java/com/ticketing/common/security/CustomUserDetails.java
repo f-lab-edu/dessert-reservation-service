5c161432e77fbb029bcc93227070ca941fcbb7fb
newdana01 <newdana01@gmail.com>
2026-02-16 00:43:44 +0900

    feat: CustomUserDetails 정의

---
+++ src/main/java/com/ticketing/common/security/CustomUserDetails.java
@@ -0,0 +1,43 @@
+package com.ticketing.common.security;
+
+import com.ticketing.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Builder
+@Getter
+public class CustomUserDetails implements UserDetails {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    public static CustomUserDetails from(User user){
+        return CustomUserDetails.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .build();
+    }
+}
