b3c83572dcf25df742cbba45e6c18af11d700e06
newdana01 <newdana01@gmail.com>
2026-02-20 16:45:32 +0900

    feat: getSubscriptionList 반환 타입 수정 (SubscriptionRes->StoreRes)

--- src/main/java/com/ticketing/service/SubscriptionService.java
+++ src/main/java/com/ticketing/service/SubscriptionService.java
@@ -1,6 +1,6 @@
 package com.ticketing.service;
 
-import com.ticketing.dto.SubscriptionRes;
+import com.ticketing.dto.StoreRes;
 
 import java.util.List;
 
@@ -10,8 +10,9 @@ public interface SubscriptionService {
      * 이미 구독 중이면 구독 취소, 구독하지 않았으면 새로 구독 추가.
      */
     void toggleSubscription(Long userId, Long storeId);
+
     /**
-     * 구독 중인 상점 리스트 조회
+     * 구독 중인 상점 리스트 조회.
      */
-    List<SubscriptionRes> getSubscriptionList(Long userId);
+    List<StoreRes> getSubscriptionList(Long userId);
 }
