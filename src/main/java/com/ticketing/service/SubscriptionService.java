e8e28ef23ef9e2d22d14c614d9ce3215d8532caa
newdana01 <newdana01@gmail.com>
2026-02-20 16:01:17 +0900

    feat: SubscriptionService - toggleSubscription(): 상점 구독 토글 - getSubscriptionList(): 구독 중인 상점 조회

---
+++ src/main/java/com/ticketing/service/SubscriptionService.java
@@ -0,0 +1,17 @@
+package com.ticketing.service;
+
+import com.ticketing.dto.SubscriptionRes;
+
+import java.util.List;
+
+public interface SubscriptionService {
+    /**
+     * 상점 구독 토글.
+     * 이미 구독 중이면 구독 취소, 구독하지 않았으면 새로 구독 추가.
+     */
+    void toggleSubscription(Long userId, Long storeId);
+    /**
+     * 구독 중인 상점 리스트 조회
+     */
+    List<SubscriptionRes> getSubscriptionList(Long userId);
+}
