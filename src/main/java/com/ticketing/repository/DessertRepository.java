38cae29665da55c5baea4dcf6d7e55098921c880
newdana01 <newdana01@gmail.com>
2026-02-22 23:39:54 +0900

    feat: updatePendingToOpenByOpenDt() 디저트 상태 변경 메서드 추가 - 업데이트한 로우 개수 반환

--- src/main/java/com/ticketing/repository/DessertRepository.java
+++ src/main/java/com/ticketing/repository/DessertRepository.java
@@ -2,9 +2,21 @@ package com.ticketing.repository;
 
 import com.ticketing.entity.Dessert;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface DessertRepository extends JpaRepository<Dessert, Long> {
     List<Dessert> findAllByStoreId(Long storeId);
+
+    /**
+     * PENDING 상태의 디저트 중 오픈 시각이 도래한 것을 OPEN 상태로 변경.
+     */
+    @Modifying
+    @Query(value = "UPDATE desserts SET open_status = 'OPEN' " +
+                   "WHERE open_status = 'PENDING' " +
+                   "AND DATE_FORMAT(open_dt, '%Y-%m-%d %H:%i') <= DATE_FORMAT(NOW(), '%Y-%m-%d %H:%i')",
+           nativeQuery = true)
+    int updatePendingToOpenByOpenDt();
 }
